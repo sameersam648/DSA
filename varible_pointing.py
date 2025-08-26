440bf02e71389b188817d4e89200124f488bc4ae
sameersam648 <154335238+sameersam648@users.noreply.github.com>
2025-08-26 12:01:31 -0600

    learned about == and is

---
+++ varible_pointing.py
@@ -0,0 +1,9 @@
+a = [1,2, 3]
+b = [1, 2, 3]
+
+print(a ==b, a is b)
+
+''' at first it will print true, 
+because the values in both a and b are same
+then it will print false as the the values at a and b 
+point to different memory locaitons'''
