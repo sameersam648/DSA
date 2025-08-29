a2bc3063401117994b34e42cfc4c5681a9a1a01b
sameersam648 <154335238+sameersam648@users.noreply.github.com>
2025-08-29 13:07:42 -0600

    palindrome solved

--- palindrome.py
+++ palindrome.py
@@ -1,9 +1,22 @@
-def is_palendrome(s):
+'''def is_palendrome(s):
     s = s.lower()
     return s == s[::-1]
 
 print(is_palendrome("Racecar"))
 print(is_palendrome("madam"))
 print(is_palendrome("sAMEER"))
+'''
 
+def is_palindrome(s):
+    s = s.lower()
+    left, right = 0, len(s) - 1
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
+
+print(is_palindrome("madam"))
+print(is_palindrome("sameer"))
 
