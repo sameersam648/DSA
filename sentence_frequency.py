2b960aa36ce9a98a2b1e4d3cd897223ba61461e5
sameersam648 <154335238+sameersam648@users.noreply.github.com>
2025-08-30 14:30:54 -0600

    sentence frequecy

---
+++ sentence_frequency.py
@@ -0,0 +1,11 @@
+sentence = "the cat sat on the mat with the cat"
+#split the sectence into words
+words = sentence.split(" ")
+
+#creating the empty dictionary
+freq = {}
+
+for word in words:
+    freq[word] = freq.get(word, 0) + 1
+
+print(freq)
