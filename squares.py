e8267054f2195b48acd44e63d360420afdc99cef
sameersam648 <154335238+sameersam648@users.noreply.github.com>
2025-08-30 13:22:01 -0600

    string extracting

---
+++ squares.py
@@ -0,0 +1,12 @@
+'''
+for i in range(0,21):
+    print(f"{i} x {i} = {i*i}")
+'''
+
+squares = [i**2 for i in range(0,21)]
+print(squares)
+
+
+#solved form my own 
+#achievement
+#1st question solved by myself
