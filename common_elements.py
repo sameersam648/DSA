6a008096867b8ed9902ed83007328d17d12ee475
sameersam648 <154335238+sameersam648@users.noreply.github.com>
2025-09-06 10:43:34 -0600

    common elements

---
+++ common_elements.py
@@ -0,0 +1,14 @@
+#find the common elements in the lists
+
+a =[1,2,3,4] 
+b = [3,4,5,6]
+
+common=[]
+
+for i in a:
+    if i in b:
+        common.append(i)
+
+print(common)
+
+#2nd problem solved by my self partially , just took a preview from the gpt then did it my self
