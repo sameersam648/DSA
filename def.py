55be12027e90a98117666d14a3e73e8bd93a1a66
sameersam648 <154335238+sameersam648@users.noreply.github.com>
2025-09-06 10:46:47 -0600

    def == default

---
+++ def.py
@@ -0,0 +1,10 @@
+#using default(def function add 2 numbers
+
+def add(a,b):
+    return a+b
+
+print(add(2,2))
+print(add(10,30))
+
+
+#again a banger , i have solved this completely by mself with out the help of gpt 
